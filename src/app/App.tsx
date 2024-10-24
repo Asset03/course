738245421b3a7d74cd0e4420bc96fdea1d7b8ce8
Asset <asset03>
2024-10-24 12:27:57 +0500

    1 topic

---
+++ src/app/App.tsx
@@ -0,0 +1,27 @@
+import React, { Suspense } from "react"
+import './styles/index.scss'
+import { Route, Routes, Link } from "react-router-dom"
+import { AboutPage } from "pages/AboutPage"
+import { MainPage } from "pages/MainPage"
+import { useTheme } from "app/providers/ThemeProvider"
+import { classNames } from "shared/lib/classNames/classNames"
+
+const App = () => {
+    const { theme, toggleTheme } = useTheme()
+
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <button onClick={toggleTheme}>Toggle</button>
+            <Link to={'/'}>Main</Link>
+            <Link to={'/about'}>About</Link>
+            <Suspense fallback={(<div>Loading...</div>)}>
+                <Routes>
+                    <Route path="/" element={<MainPage />} />
+                    <Route path="/about" element={<AboutPage />} />
+                </Routes>
+            </Suspense>
+        </div>
+    )
+}
+
+export default App
