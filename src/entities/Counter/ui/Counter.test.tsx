7f94bbe28cd7850921483dcba65d09abf5726fdd
Asset <asset03>
2025-02-12 18:18:08 +0500

    add: store,tests

---
+++ src/entities/Counter/ui/Counter.test.tsx
@@ -0,0 +1,29 @@
+import { screen } from '@testing-library/react';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
+import { userEvent } from '@storybook/testing-library';
+import { Counter } from './Counter';
+
+describe('Counter', () => {
+    test('test render', () => {
+        componentRender(<Counter />, {
+            initialState: { counter: { value: 10 } },
+        });
+        expect(screen.getByTestId('value-id')).toHaveTextContent('10');
+    });
+
+    test('increment', () => {
+        componentRender(<Counter />, {
+            initialState: { counter: { value: 10 } },
+        });
+        userEvent.click(screen.getByTestId('increment-id'));
+        expect(screen.getByTestId('value-id')).toHaveTextContent('11');
+    });
+
+    test('decrement', () => {
+        componentRender(<Counter />, {
+            initialState: { counter: { value: 10 } },
+        });
+        userEvent.click(screen.getByTestId('decrement-id'));
+        expect(screen.getByTestId('value-id')).toHaveTextContent('9');
+    });
+});
