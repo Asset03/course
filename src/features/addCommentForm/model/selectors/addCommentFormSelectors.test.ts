3930f727ae7e6776550ee05a3eb6f3728f9a5b92
Asset <asset03>
2025-06-18 17:56:09 +0500

    feat: added test for add comment form

---
+++ src/features/addCommentForm/model/selectors/addCommentFormSelectors.test.ts
@@ -0,0 +1,25 @@
+import { StateSchema } from 'app/providers/StoreProvider';
+import { getAddCommentFormText, getAddCommentFormError } from './addCommentFormSelectors';
+
+describe('getAddCommentFormSelectors.test', () => {
+    test('should return text', () => {
+        const state: DeepPartial<StateSchema> = {
+            addCommentForm: {
+                text: 'Hello World!',
+            },
+        };
+        expect(getAddCommentFormText(state as StateSchema)).toEqual('Hello World!');
+    });
+    test('should return error', () => {
+        const state: DeepPartial<StateSchema> = {
+            addCommentForm: {
+                error: 'error',
+            },
+        };
+        expect(getAddCommentFormError(state as StateSchema)).toEqual('error');
+    });
+    test('should work with empty state', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getAddCommentFormError(state as StateSchema)).toEqual(undefined);
+    });
+});
---
+++ src/features/addCommentForm/model/slices/addCommentFormSlice.test.ts
@@ -0,0 +1,12 @@
+import { AddCommentFormSchema } from '../types/addCommentForm';
+import { addCommentFormActions, addCommentFormReducer } from './addCommentFormSlice';
+
+describe('addCommentFormSlice.test.ts', () => {
+    test('test set text', () => {
+        const state: DeepPartial<AddCommentFormSchema> = { text: '' };
+        expect(addCommentFormReducer(
+            state as AddCommentFormSchema,
+            addCommentFormActions.setText('Hello World!'),
+        )).toEqual({ text: 'Hello World!' });
+    });
+});
