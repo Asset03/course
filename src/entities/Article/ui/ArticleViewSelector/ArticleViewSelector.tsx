dde36e37c09f081866df0d700324e9210524915a
Asset <asset03>
2025-06-23 15:28:39 +0500

    lesson: 55

---
+++ src/entities/Article/ui/ArticleViewSelector/ArticleViewSelector.tsx
@@ -0,0 +1,50 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import ListIcon from 'shared/assets/icons/list-24-24.svg';
+import TiledIcon from 'shared/assets/icons/tiled-24-24.svg';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { Icon } from 'shared/ui/Icon/Icon';
+import { ArticleView } from '../../model/types/article';
+import cls from './ArticleViewSelector.module.scss';
+
+interface ArticleViewSelectorProps {
+    className?: string;
+    view: ArticleView;
+    onViewClick?: (view: ArticleView) => void;
+}
+
+const viewTypes = [
+    {
+        view: ArticleView.SMALL,
+        icon: TiledIcon,
+    },
+    {
+        view: ArticleView.BIG,
+        icon: ListIcon,
+    },
+];
+
+export const ArticleViewSelector = (props: ArticleViewSelectorProps) => {
+    const { className, view, onViewClick } = props;
+    const { t } = useTranslation();
+
+    const onClick = (newView: ArticleView) => () => {
+        onViewClick?.(newView);
+    };
+
+    return (
+        <div className={classNames(cls.ArticleViewSelector, {}, [className])}>
+            {viewTypes.map((item, index) => (
+                <Button
+                    theme={ButtonTheme.CLEAR}
+                    onClick={onClick(item.view)}
+                >
+                    <Icon
+                        Svg={item.icon}
+                        className={classNames('', { [cls.notSelected]: item.view !== view })}
+                    />
+                </Button>
+            ))}
+        </div>
+    );
+};
