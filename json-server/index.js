9702feec261f4bb6fc8c95c1fcb751625b33dbe8
Asset <asset03>
2025-08-18 19:48:10 +0500

    add: https for json-server

--- json-server/index.js
+++ json-server/index.js
@@ -1,6 +1,13 @@
 const fs = require('fs');
 const jsonServer = require('json-server');
 const path = require('path');
+const https = require('https');
+
+// Load the certificate and private key
+const options = {
+    key: fs.readFileSync(path.resolve(__dirname, 'key.pem')), // Path to your private key file
+    cert: fs.readFileSync(path.resolve(__dirname, 'cert.pem')), // Path to your certificate file
+};
 
 const server = jsonServer.create();
 
@@ -21,7 +28,9 @@ server.use(async (req, res, next) => {
 server.post('/login', (req, res) => {
     try {
         const { username, password } = req.body;
-        const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'));
+        const db = JSON.parse(
+            fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'),
+        );
         const { users = [] } = db;
 
         const userFromBd = users.find(
@@ -52,6 +61,8 @@ server.use((req, res, next) => {
 server.use(router);
 
 // запуск сервера
-server.listen(8000, () => {
-    console.log('server is running on 8000 port');
+
+const httpsServer = https.createServer(options, server);
+httpsServer.listen(443, () => {
+    console.log('server is running on 443 port');
 });
